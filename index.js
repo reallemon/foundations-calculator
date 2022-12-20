d3625f70932022023a58e04faa8642b4ac8ae7d3
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-20 00:19:36 -0700

    Feat: Add function to interpret operations

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      console.error('Please use a valid operator');
+  }
+}
